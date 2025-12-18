3407e00fc1e667287c6c66c28d5fe07fea913ab0
ThinilSandaru <thinilsandaru@gmail.com>
2025-12-18 23:19:27 +0530

    Made to add images

--- backend/src/main/java/com/example/backend/Services/AddNewBookService.java
+++ backend/src/main/java/com/example/backend/Services/AddNewBookService.java
@@ -6,10 +6,18 @@ import com.example.backend.Model.BookCopy;
 import com.example.backend.Repository.BookCopyRepository;
 import com.example.backend.Repository.BookRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 @Service
 public class AddNewBookService {
@@ -25,12 +33,27 @@ public class AddNewBookService {
 		this.bookCopyRepository=bookCopyRepository;
 	}
 
-	public Map<String,String> addNewBook(NewBookDTO newBookDTO){
+	public Map<String,String> addNewBook(NewBookDTO newBookDTO, MultipartFile image) throws IOException {
+
+		String uploadDir="uploads/";
+		File folder=new File(uploadDir);
+
+		if(!folder.exists()){
+			folder.mkdirs();
+		}
+
+		String fileName = UUID.randomUUID().toString() + "_" + image.getOriginalFilename();
+		Path filePath = Paths.get(uploadDir + fileName);
+		Files.copy(image.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+		String imageUrl = "http://localhost:8080/uploads/" + fileName;
+
+
+
 		Book book=new Book();
 		book.setTitle(newBookDTO.getTitle());
 		book.setAuthor(newBookDTO.getAuthor());
 		book.setPrice(newBookDTO.getPrice());
-
+		book.setImageUrl(imageUrl);
 		List<BookCopy> copies=new ArrayList<>();
 		int n_of_copies=newBookDTO.getCopies();
 
